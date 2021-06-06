e970f3c888b27931a2cce689f5e8e83ee19983fe
Robby <ta7382@gmail.com>
2021-06-06 16:03:58 +0800

    Add map and direction

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -8,6 +8,12 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ExploreContainerComponent implements OnInit {
   @Input() name: string;
 
+  public lat = 24.799448;
+  public lng = 120.979021;
+
+  public origin: any = 'Taichung Railway Station, Taiwan';
+  public destination: any = 'Wenxin Forest Park, Taiwan';
+
   constructor() { }
 
   ngOnInit() {}
